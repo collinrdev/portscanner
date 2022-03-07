97518dc3314e3d0cdb523c8a42e0eea26a0d0532
Collin R <collin.rittschof@gmail.com>
2022-03-07 13:00:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import sys
+import socket
+if len (sys.argv) >= 2:
+	target = socket.gethostbyname(sys.argv[1])
+	#print(target)
+else:
+	print('invalid argument')
+
+protocol = sys.argv[2]
+
+if protocol == 't':
+	for port in range(1,65535):
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket.setdefaulttimeout(1)
+		output = s.connect_ex((target,port))
+		if output == 0:
+			print("open: {}".format(port))
+		s.close()
+elif protocol == 'u':
+	for port in range(1,65535):
+		s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		socket.setdefaulttimeout(1)
+		output = s.connect_ex((target,port))
+		if output == 0:
+			print("open: {}".format(port))
+		s.close()
